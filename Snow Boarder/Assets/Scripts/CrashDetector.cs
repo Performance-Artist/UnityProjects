a8af1aa7b76b7ace635c055306d45f3a1514ba5e
Performance-Artist <bitca.boris@gmail.com>
2022-11-18 22:48:26 +0200

    Upload Snow Boader Project

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            GetComponent<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
