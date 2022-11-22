57c63963e821038e771551e021dd422b09227eed
Performance-Artist <bitca.boris@gmail.com>
2022-11-22 12:31:29 +0200

    Polished attacking state

--- Third Person Combat/Assets/Scripts/Combat/WeaponDamage.cs
+++ Third Person Combat/Assets/Scripts/Combat/WeaponDamage.cs
@@ -7,6 +7,8 @@ public class WeaponDamage : MonoBehaviour
 {
     [SerializeField] private Collider myCollider;
 
+    private int damage;
+
     private List<Collider> alreadyCollidedWith = new List<Collider>();
 
     private void OnEnable()
@@ -24,7 +26,12 @@ public class WeaponDamage : MonoBehaviour
 
         if (other.TryGetComponent<Health>(out Health health))
         {
-            health.DealDamage(10);
+            health.DealDamage(damage);
         }
+    }
+
+    public void SetAttack(int damage)
+    {
+        this.damage = damage;
     }
 }
